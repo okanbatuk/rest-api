9d242a9cffb3c1c054095dc121582a267fc2e865
myrnz <okanbatuk@protonmail.com>
2022-08-11 16:39:45 +0300

    Express config was created

---
+++ src/config/express.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+//Middleware
+app.use(morgan("dev"));
+
+// Parse body params and attach them to req.body
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// TODO: Route conf will be created here
